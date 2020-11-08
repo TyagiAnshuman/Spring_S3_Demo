eeba609fe3c073738eef09c4b5aee0d5cecac7a6
TyagiAnshuman <tyagi.anshuman@gmail.com>
2020-11-08 14:11:14 +0530

    My_Firt_Spring

---
+++ src/main/java/com/aws/anshuman/AnshumanApplication.java
@@ -0,0 +1,13 @@
+package com.aws.anshuman;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnshumanApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AnshumanApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/aws/anshuman/AnshumanApplicationTests.java
@@ -0,0 +1,13 @@
+package com.aws.anshuman;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AnshumanApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
